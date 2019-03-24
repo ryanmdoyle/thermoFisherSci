237ef724c375903873b870c4bd7db3fc3ceafe67
Ryan <ryandoyle@me.com>
2019-03-24 15:01:47 -0700

    modified languages and permissions

--- backend/generated/prisma-client/index.js
+++ backend/generated/prisma-client/index.js
@@ -15,6 +15,10 @@ var models = [
   {
     name: "Part",
     embedded: false
+  },
+  {
+    name: "Edits",
+    embedded: false
   }
 ];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
---
+++ frontend/pages/hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const hello = () => {
+  return (
+    <div>
+      <p>Hello!</p>
+    </div>
+  );
+};
+
+export default hello;
