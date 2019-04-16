61a4d3a30e9af46aabec2f4d4a61170cd65973f3
Ryan <ryandoyle@me.com>
2019-04-15 19:46:09 -0700

    markdown preview component

--- frontend/components/MarkdownField.js
+++ frontend/components/MarkdownField.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import { markdown } from 'markdown';
+import marked from 'marked';
 import styled from 'styled-components';
+import sanitizeHTML from 'sanitize-html';
 
 const MarkdownTextField = styled.div`
   textarea {
-    width: 50%;
+    width: 40%;
     min-height: 250px;
+    display: inline-block;
   }
-  div {
-    width: 50%;
+  .md-output {
+    width: 40%;
     padding-left: 50px;
+    display: inline-block;
   }
 `;
+
+marked.setOptions({
+  sanitize: true,
+  sanitizer: sanitizeHTML(),
+})
 
 class MarkdownField extends Component {
   state = {
@@ -20,7 +29,14 @@ class MarkdownField extends Component {
   };
 
   inputToState = (e) => {
-    this.setState({[e.target.name]: e.target.value });
+    this.setState({
+      [e.target.name]: e.target.value,
+      markdown: marked(e.target.value),
+    });
+  };
+
+  displayMD = () => {
+    return { __html: sanitizeHTML(this.state.markdown) }
   };
 
   render() {
@@ -29,11 +45,12 @@ class MarkdownField extends Component {
         <h2>Label here</h2>
         <textarea
           type='text'
-          name='chineseLong_zh_cn'
-          placeholder='chineseLong_zh_cn'
+          name='input'
+          placeholder='input'
           onChange={this.inputToState}
         ></textarea>
-      </MarkdownTextField>
+        <div dangerouslySetInnerHTML={this.displayMD()}></div>
+      </MarkdownTextField >
     );
   }
 }
