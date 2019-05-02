b3e543f053880021a7f41ca40010298ebe60ed1f
Ryan Doyle <ryandoyle@me.com>
2019-05-02 14:25:32 -0700

    button css background & dropdown arrows

--- frontend/components/Logout.js
+++ frontend/components/Logout.js
@@ -4,6 +4,7 @@ import Router from 'next/router';
 import React from 'react';
 import gql from 'graphql-tag';
 import { CURRENT_USER_QUERY } from './User';
+import styled from 'styled-components';
 
 const LOGOUT_MUTATION = gql`
    mutation LOGOUT_MUTATION {
@@ -13,14 +14,18 @@ const LOGOUT_MUTATION = gql`
    }
 `;
 
+const LogoutButtonStyled = styled.button`
+  background-color: white;
+`;
+
 const Logout = (props) => {
   return (
     <Mutation mutation={LOGOUT_MUTATION} refetchQueries={[{ query: CURRENT_USER_QUERY }]}>
       {(logout) => (
         // run logout mutation and routes user to login page
-        <button onClick={() => { logout(); Router.push('/login') }}>
+        <LogoutButtonStyled onClick={() => { logout(); Router.push('/login') }}>
           <a>Log Out</a>
-        </button>
+        </LogoutButtonStyled>
       )}
     </Mutation>
   );
--- frontend/components/UserPermissions.js
+++ frontend/components/UserPermissions.js
@@ -7,7 +7,7 @@ const DropdownStyled = styled.div`
     box-shadow: 0 0 0rem 0.07rem ${props => props.theme.black};
     border-radius: 0.2rem;
     margin-bottom: 1rem;
-    padding: 0.2rem 1rem 0.5rem 1rem;
+    padding: 0 1rem 0 1rem;
   }
   .user__container:hover {
     z-index: 300;
@@ -15,10 +15,33 @@ const DropdownStyled = styled.div`
     box-shadow: 0 0 0.2rem 0.1rem ${props => props.theme.red};
   }
 
+  h4 {
+    margin: 0 0;
+    padding: 1rem 0;
+  }
+
   ul {
     border-top: 1px solid black;
     padding: 1rem 0.2rem 0.2rem 2rem;
   }
+
+  i {
+  border: solid black;
+  border-width: 0 3px 3px 0;
+  display: inline-block;
+  padding: 0.25rem;
+  margin-right: 1rem;
+}
+
+.up {
+  transform: rotate(-135deg);
+  -webkit-transform: rotate(-135deg);
+}
+
+.down {
+  transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
+}
 
   @media screen and (max-width:600px) {
     .part-header__number {
@@ -44,11 +67,21 @@ class UserPermissions extends Component {
   render() {
     return (
       <DropdownStyled>
-        <div className='user__container' onClick={() => this.togglePermissions()}>
-          <h4>{this.props.name}</h4>
+        <div className='user__container'>
+          <h4  onClick={() => this.togglePermissions()}>
+            {this.state.showPermissions ? <i className='up'></i>:  <i className='down'></i>}
+            {this.props.name}
+          </h4>
           {this.state.showPermissions && 
             <ul>
-             {this.props.permissions}
+             {this.props.permissions.map(permission => {
+               return (
+                 <>
+                  <label><input type='checkbox'></input>{permission}</label>
+                  <br></br>
+                 </>
+               )
+             })}
             </ul>
           }
         </div> 
