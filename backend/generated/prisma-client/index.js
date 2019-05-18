03d3278319e3e80b5d5458e3cca28a947b6e9597
Ryan <ryandoyle@me.com>
2019-05-18 16:09:22 -0700

    updates & prod prep

--- backend/generated/prisma-client/index.js
+++ backend/generated/prisma-client/index.js
@@ -20,7 +20,7 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `https://us1.prisma.sh/ryan-doyle/thermofisher-inventory/dev`,
-  secret: `drinkingPhilzCoffee`
+  endpoint: `https://thermo-production-7c63fed9d5.herokuapp.com/thermo-db-prod/prod`,
+  secret: `${process.env["DB_SECRET"]}`
 });
 exports.prisma = new exports.Prisma();
